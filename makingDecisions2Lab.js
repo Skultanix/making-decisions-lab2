8bc6f40394e0df4a6bea2a6956adb630faf152cf
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-02-10 12:28:48 -0700

    Problem eight complete.

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -122,6 +122,13 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 //Code Here
 
+for( let marmot = 0; marmot < 12; marmot++) {
+  if(listOfNumbers[marmot] % 3 === 0) {
+    console.log(`${listOfNumbers[marmot]} is divisible by 3`)
+  } else {
+    
+  }
+} 
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
